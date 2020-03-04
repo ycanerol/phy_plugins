d38b82d2d8d33d9de5d93462d3bf8118fb517f35
szapp <dev.szapp@gmail.com>
2020-03-04 09:45:53 +0100

    Move ReverseSelection into SelectionOptions

--- SelectionOptions.py
+++ SelectionOptions.py
@@ -1,9 +1,33 @@
 """
 Additional selection options
+============================
+
+Reverse the cluster selection
+-----------------------------
+
+The order of the selection (evident by the coloring of the selection) is
+reversed. This is especially useful for finding the first selected
+cluster in the trace view when one of the clusters has few spikes.
+
+Both selections from the cluster view and the similarity view are
+considered.
+
+Triggering the action twice results in the original selection. If
+there were more selections than one cluster view - similarity view pair,
+all selections will be moved to cluster view and no longer in the
+similarity view.
+
+
+Select the newest (non noise) cluster
+-------------------------------------
 
 Select the cluster with the highest id (among non-noise clusters). This
 is useful after performing an action and losing track of the recently
 created cluster.
+
+
+Select all unsorted clusters in current channel
+-----------------------------------------------
 
 Select all yet unsorted clusters within the current channel. This is
 useful to quickly see all clusters side-by-side.
@@ -20,6 +44,34 @@ class SelectionOptions(IPlugin):
     def attach_to_controller(self, controller):
         @connect
         def on_gui_ready(sender, gui):
+            @controller.supervisor.actions.add(shortcut='alt+y',
+                                               name='Reverse selection',
+                                               menu='Sele&ct')
+            def reverseselection():
+                """Reverse the current cluster selection order"""
+                sup = controller.supervisor
+
+                if len(sup.selected) < 2:
+                    return
+
+                if (len(sup.selected) == 2
+                        and len(sup.selected_clusters) == 1
+                        and len(sup.selected_similar) == 1):
+                    # Switch cluster and similarity selection
+                    state = (sup.selected_similar, None,
+                             sup.selected_clusters, None)
+                else:
+                    # Move the selection to the cluster view only
+                    state = (sup.selected[::-1], None, None, None)
+
+                logger.info('Reverse selection of clusters from %s to %s.',
+                            ', '.join(map(str, sup.selected)),
+                            ', '.join(map(str, sup.selected[::-1])))
+
+                # Let the TaskLogger take care of making the selections
+                sup.task_logger._select_state(state)
+                sup.task_logger.process()
+
             @controller.supervisor.actions.add(shortcut='shift+pgdown',
                                                name='Select newest cluster',
                                                menu='Sele&ct')
