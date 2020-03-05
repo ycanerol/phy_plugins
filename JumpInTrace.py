622abcaf959ec420561c6d4ac4715115eadb0549
szapp <dev.szapp@gmail.com>
2020-03-05 13:31:47 +0100

    Move JumpInTrace entries to trace view menu

--- JumpInTrace.py
+++ JumpInTrace.py
@@ -6,6 +6,7 @@ import logging
 import numpy as np
 from phy import IPlugin, connect
 from phy.cluster.views.trace import TraceView as TraceView
+
 logger = logging.getLogger('phy')
 
 
@@ -14,48 +15,42 @@ class JumpInTrace(IPlugin):
     def attach_to_controller(self, controller):
 
         @connect
-        def on_gui_ready(sender, gui):
+        def on_view_attached(view, gui):
+            if isinstance(view, TraceView):
 
-            trace_view = gui.get_view(TraceView)
-            go_to = trace_view.go_to
+                def _jump_to_spike(delta=+1):
+                    """
+                    Move within the spikes of any of the selected clusters.
+                    """
+                    time = view.time  # Current position
 
-            def _jump_to_spike(delta=+1):
-                """
-                Move within the spikes of any of the selected clusters.
-                """
-                time = trace_view.time  # Current position
+                    selected = controller.supervisor.selected
+                    spt = np.array([])
+                    for sel in selected:
+                        spt = np.hstack((spt,
+                                         (controller.get_spike_times(sel))))
+                    spike_times = np.sort(spt)
+                    ind = np.searchsorted(spike_times, time)
+                    n = len(spike_times)
+                    target = spike_times[(ind + delta) % n]
+                    logger.debug('Jump with %+d to one of the spikes from '
+                                 'clusters %s. Jumped from %.5f to %.5f.',
+                                 delta, ', '.join(map(str, selected)), time,
+                                 target)
+                    view.go_to(target)
 
-                selected = controller.supervisor.selected
-                spt = np.array([])
-                for sel in selected:
-                    spt = np.hstack((spt, (controller.get_spike_times(sel))))
-                spike_times = np.sort(spt)
-                ind = np.searchsorted(spike_times, time)
-                n = len(spike_times)
-                target = spike_times[(ind + delta) % n]
-                logger.debug(f"Jump with {delta} to one of the spikes from"
-                             f" clusters {selected}. Jumped from {time:.5f} "
-                             f"to {target:.5f}.")
-                go_to(target)
+                @view.actions.add(shortcut='shift+alt+pgdown',
+                                  name='Jump to next spike')
+                def jump_to_next_spike():
+                    """
+                    Go to next spike from any selected cluster.
+                    """
+                    _jump_to_spike(+1)
 
-            @controller.supervisor.actions.add(shortcut='shift+alt+pgdown',
-                                               name='Next spike',
-                                               menu='&View',
-                                               #submenu='TraceView',
-                                               )
-            def jump_to_next_spike():
-                """
-                Go to next spike from any selected cluster.
-                """
-                _jump_to_spike(+1)
-
-            @controller.supervisor.actions.add(shortcut='shift+alt+pgup',
-                                               name='Previous spike',
-                                               menu='&View',
-                                               #submenu='TraceView',
-                                               )
-            def jump_to_prev_spike():
-                """
-                Go to previous spike from any selected cluster.
-                """
-                _jump_to_spike(-1)
+                @view.actions.add(shortcut='shift+alt+pgup',
+                                  name='Jump to previous spike')
+                def jump_to_prev_spike():
+                    """
+                    Go to previous spike from any selected cluster.
+                    """
+                    _jump_to_spike(-1)
