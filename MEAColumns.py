5184e3957b552c9bf475d1b3b5478cd05a0c9938
szapp <dev.szapp@gmail.com>
2020-03-09 16:19:45 +0100

    Make MEAColumns compatible with KS1, add safety checks
    Instead of removing 'amp', remove 'Amplitude' (created by Kilosort 2)

--- MEAColumns.py
+++ MEAColumns.py
@@ -1,13 +1,17 @@
 """Remove unneccessary columns for curating MEA data"""
 
 from phy import IPlugin, connect
+import logging
+
+logger = logging.getLogger('phy')
 
 
 class MEAColumns(IPlugin):
     def attach_to_controller(self, controller):
         @connect
         def on_controller_ready(sender):
-            columns_to_remove = ['sh', 'depth', 'amp']
+            columns_to_remove = ['sh', 'depth', 'Amplitude']
             for col in columns_to_remove:
-                controller.supervisor.columns.remove(col)
-
+                if col in controller.supervisor.columns:
+                    logger.debug('Remove column `%s`.', col)
+                    controller.supervisor.columns.remove(col)
