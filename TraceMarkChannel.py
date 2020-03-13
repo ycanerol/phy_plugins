d269b2c0281fd39c658a693b20d5f9853e7279cb
szapp <dev.szapp@gmail.com>
2020-03-13 17:51:10 +0100

    Update labels on go_to in TraceMarkChannel

--- TraceMarkChannel.py
+++ TraceMarkChannel.py
@@ -8,24 +8,17 @@ from phy import IPlugin, connect
 
 class TraceMarkChannel(IPlugin):
     # Highlight color
-    text_color = (1, 0, 0, 1)
+    color = (1, 0, 0, 1)
 
     def attach_to_controller(self, controller):
         @connect
         def on_view_attached(view, gui):
             if isinstance(view, TraceView):
-                @connect(sender=controller.supervisor)
-                def on_select(sender, cluster_ids=None, **kwargs):
-                    if not cluster_ids:
-                        return
+                # Add attribute
+                view.ch = []
 
-                    # Get selected channels
-                    channels = set(
-                        int(controller.supervisor.get_cluster_info(c)['ch'])
-                        for c in cluster_ids
-                    )
-
-                    # Mimic _plot_labels
+                # Overwrite the label plotting of the view
+                def _plot_labels(traces):
                     view.text_visual.reset_batch()
                     for ch in range(view.n_channels):
                         bi = view.channel_y_ranks[ch]
@@ -36,6 +29,22 @@ class TraceMarkChannel(IPlugin):
                             anchor=[+1., 0],
                             data_bounds=view.data_bounds,
                             box_index=bi,
-                            color=self.text_color if ch in channels else None
+                            color=self.color if ch_label in view.ch else None
                         )
                     view.canvas.update_visual(view.text_visual)
+                view._plot_labels = _plot_labels
+
+                @connect(sender=controller.supervisor)
+                def on_select(sender, cluster_ids=None, **kwargs):
+                    if not cluster_ids:
+                        view.ch = []
+                        return
+
+                    # Get selected channels
+                    view.ch = set(
+                        controller.supervisor.get_cluster_info(c)['ch']
+                        for c in cluster_ids
+                    )
+
+                    # Update highlighting
+                    view._plot_labels(None)
