f01baa94eeea81fdf13c7fc5165dcf3d9c0181f5
szapp <dev.szapp@gmail.com>
2020-03-17 10:58:45 +0100

    Mark newly created clusters in MarkChannel

--- MarkChannel.py
+++ MarkChannel.py
@@ -6,6 +6,9 @@ import numpy as np
 from phy import IPlugin, connect
 from phy.cluster.supervisor import ClusterView
 from phy.utils.color import selected_cluster_color
+import logging
+
+logger = logging.getLogger('phy')
 
 
 class MarkChannel(IPlugin):
@@ -41,13 +44,33 @@ class MarkChannel(IPlugin):
                 js = """
                     var ll = """ + str(clust) + """;
                     var itms = document.getElementsByTagName("tr");
+                    var chng = []
                     for (var i = 0; i < itms.length; i++) {
                         var c_id = itms[i].getAttribute('data-_id');
+
+                        // New clusters do not have this attribute
+                        if (!c_id) {
+                            c_id = itms[i].getElementsByClassName('id')
+                            if (c_id.length) {
+                                c_id = c_id[0].innerHTML;
+                            } else {
+                                continue;
+                            }
+                        };
+
                         if (Object.keys(ll).indexOf(c_id) >= 0) {
                             itms[i].style.background = ll[c_id];
+                            chng.push(c_id);
                         } else {
                             itms[i].style.background = '';
                         }
                     }
+
+                    // Report highlighted clusters to callback function
+                    chng
                 """
-                view.eval_js(js)
+
+                def report(obj):
+                    logger.debug('Highlighted clusters  %s.', ', '.join(obj))
+
+                view.eval_js(js, callback=report)
